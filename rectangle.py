965d6ef0b8214713d89ae6869885ef4ca28cda0f
Christopher John Olsson <36557468+cjolsson@users.noreply.github.com>
2025-02-04 09:55:16 +1100

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -14,39 +14,54 @@ from   tkinter import filedialog, messagebox
 #from   pylab import rcParams
 from   PIL import Image, ImageTk
 
-ix, iy = -1, -1
+
+fx, fy, ix, iy = -1, -1, -1, -1
 drawing = False
 
 def draw_Rectangle(event, x, y, flags, param):
 
-    global ix, iy, drawing, img
+#   global img, ix, iy, fx, fy, drawing
+    global ix, iy, fx, fy, drawing
 
     if   event == cv2.EVENT_LBUTTONDOWN:
 
         drawing = True
         ix = x
-        iy = y			 
+        iy = y
 
     elif event == cv2.EVENT_MOUSEMOVE:
     
         if drawing:
 
-            cv2.rectangle(img, pt1 =(ix, iy), pt2 =(x, y), color =(0, 255, 255), thickness = -1)
+            cv2.rectangle(img, pt1 = (ix, iy), pt2 =(x, y), color = (0, 0, 255), thickness = 2)
+            cv2.rectangle(img, pt1 = (ix, iy), pt2 =(x, y), color = (0, 0, 0),   thickness = -1)
 
     elif event == cv2.EVENT_LBUTTONUP:
 
         drawing = False
-        cv2.rectangle(img, pt1 =(ix, iy), pt2 =(x, y), color =(0, 255, 255), thickness = 2)
-    
+        cv2.rectangle(img, pt1 = (ix, iy), pt2 =(x, y), color = (0, 0, 255), thickness = 2)
+        fx = x
+        fy = y
+        
 img = np.zeros((512, 700 ,3), np.uint8) # Create a black image
 
 cv2.namedWindow("Title of Popup Window", flags = cv2.WINDOW_GUI_EXPANDED) # Display a window the size of which can be manually changed
 
 cv2.setMouseCallback("Title of Popup Window", draw_Rectangle) # Connect the mouse button to our callback function
-                
+
 while True:
 
     cv2.imshow("Title of Popup Window", img) # Display the image
+    
+    if cv2.waitKey(10) & 0xFF == 27: # Break the loop when the Esc key is pressed
+        break
+
+if ix > fx:
+    ix, fx = fx, ix
+if iy > fy:
+    iy, fy = fy, iy
+    
+cv2.destroyAllWindows() # Close all windows
     
     if cv2.waitKey(10) & 0xFF == 27: # Break the loop when the Esc key is pressed
         break
